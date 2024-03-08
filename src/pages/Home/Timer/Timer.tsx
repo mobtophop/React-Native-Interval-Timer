ce861c7cb12b0f18d049332c38a0af87ee4971ab
mobtophop <mobtophop@gmail.com>
2024-03-08 13:13:38 +0200

    added working library for Timer

--- src/pages/Home/Timer/Timer.tsx
+++ src/pages/Home/Timer/Timer.tsx
@@ -1,14 +1,20 @@
 import React, {useState} from 'react';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
-import {colors} from '@components/colors.ts';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import Pause from '@icons/pause.svg';
 import Play from '@icons/play.svg';
+import {CountdownCircleTimer} from 'react-native-countdown-circle-timer';
 
 export const Timer = () => {
 	const [isPlaying, setIsPlaying] = useState(false);
 	return (
-		<View>
-			<View style={styles.shadow}></View>
+		<View style={styles.container}>
+			<CountdownCircleTimer
+				isPlaying
+				duration={7}
+				colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+				colorsTime={[7, 5, 2, 0]}>
+				{({remainingTime}) => <Text>{remainingTime}</Text>}
+			</CountdownCircleTimer>
 			<TouchableOpacity
 				style={styles.button}
 				onPress={() => setIsPlaying(!isPlaying)}>
@@ -20,27 +26,24 @@ export const Timer = () => {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: colors.main,
-	},
-	shadow: {
-		position: 'absolute',
-		width: 80,
-		height: 80,
-		borderRadius: 50,
-		opacity: 0.1,
-		backgroundColor: 'purple',
-		transform: [{translateX: -5}, {translateY: -5}],
 	},
 	button: {
+		marginTop: 40,
+		shadowColor: 'purple',
+		shadowOffset: {
+			width: 0,
+			height: 3,
+		},
+		shadowRadius: 10,
+		shadowOpacity: 0.9,
+		elevation: 20,
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center',
 		width: 70,
 		height: 70,
-		padding: 15,
+		padding: 20,
 		backgroundColor: 'purple',
 		borderRadius: 50,
 		zIndex: 1,
